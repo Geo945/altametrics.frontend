db97206ac75d467a8ba91688888bdadb0a630de0
Geo <georgeX_D@yahoo.com>
2023-11-12 17:49:22 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,17 @@
-import React from 'react';
-import { Modal, Box } from '@mui/material';
-import ModalHeader from './ModalHeader';
-import ModalFooter from './ModalFooter';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {Provider} from 'react-redux'
+import {store} from "./redux/store/store";
 
-import * as styles from './styles';
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 
-type ModalPopUpProps = {
-    open: boolean;
-    handleClose: () => void;
-    handleSubmit: () => void;
-    title: string;
-    children: JSX.Element;
-    submitBtnLabel: string;
-    submitBtnColor?: string;
-    cancelBtnLabel?: string;
-    isSubmitDisabled?: boolean;
-    center?: boolean;
-};
-
-const ModalPopUp = ({
-    open,
-    handleClose,
-    handleSubmit,
-    title,
-    children,
-    submitBtnLabel,
-    submitBtnColor = 'primary',
-    cancelBtnLabel,
-    isSubmitDisabled,
-    center = true,
-}: ModalPopUpProps) => {
-    return (
-        <Modal
-            data-testid="modal"
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={open}
-            onClose={handleClose}
-        >
-            <Box sx={styles.box}>
-                <ModalHeader
-                    title={title}
-                    handleClose={handleClose}
-                    center={center}
-                />
-                {children}
-                <ModalFooter
-                    handleClose={handleClose}
-                    handleSubmit={handleSubmit}
-                    submitBtnLabel={submitBtnLabel}
-                    submitBtnColor={submitBtnColor}
-                    cancelBtnLabel={cancelBtnLabel}
-                    isSubmitDisabled={isSubmitDisabled}
-                />
-            </Box>
-        </Modal>
-    );
-};
-
-export default ModalPopUp;
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+)
